8388d8dfbc19e37286baf79614e2f62dedb0a519
wildloop <mariusz@olejnik.net>
2018-05-22 13:35:10 +0200

    test.sh fix
    OCR Engine modes (--oem) set to 3 - Default, based on what is available.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ TASK_TMP_DIR=TASK_$$_$(date +"%N")
 echo "====== TASK $TASK_TMP_DIR started ======"
 docker exec -it t4re mkdir \-p ./$TASK_TMP_DIR/
 docker cp ./ocr-files/phototest.tif t4re:/home/work/$TASK_TMP_DIR/
-docker exec -it t4re /bin/bash -c "mkdir -p ./$TASK_TMP_DIR/out/; cd ./$TASK_TMP_DIR/out/; tesseract ../phototest.tif phototest -l eng --psm 1 --oem 2 txt pdf hocr"
+docker exec -it t4re /bin/bash -c "mkdir -p ./$TASK_TMP_DIR/out/; cd ./$TASK_TMP_DIR/out/; tesseract ../phototest.tif phototest -l eng --psm 1 --oem 3 txt pdf hocr"
 mkdir -p ./ocr-files/output/$TASK_TMP_DIR/
 docker cp t4re:/home/work/$TASK_TMP_DIR/out/ ./ocr-files/output/$TASK_TMP_DIR/
 docker exec -it t4re rm \-r ./$TASK_TMP_DIR/
